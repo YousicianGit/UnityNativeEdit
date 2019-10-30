694f13f8c7136b9dac12ae70417fee0848d160d4
Juho Iso-Markku <juho@yousician.com>
2019-10-30 14:10:22 +0200

    Revert "Touch firstresponder when new EditBox is created to properly init it on os level"
    This reverts commit 8059a4a4e0b47cfdf2c73a463f84a52138c3b583.

--- release/NativeEditPlugin/Plugins/iOS/EditBox_iOS.m
+++ release/NativeEditPlugin/Plugins/iOS/EditBox_iOS.m
@@ -478,8 +478,6 @@ bool approxEqualFloat(float x, float y)
         editView = textField;
     }
     [viewPlugin addSubview:editView];
-    [editView becomeFirstResponder];
-    [editView resignFirstResponder];
 }
 
 -(void) setText:(JsonObject*)json
